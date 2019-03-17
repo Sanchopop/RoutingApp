b51f091e3f6b195e78e30a067eb3f28f275a271e
Alexander Popov <Sanchopop@yandex.ua>
2019-03-17 22:03:34 +0200

    Added button and toastr at home-page

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
+declare let toastr: any;
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
@@ -10,7 +11,12 @@ export class HomePageComponent implements OnInit {
   arrPhones = [{
     imgUrl: ''
   }];
+  arrPackPhones = [{
+    imgUrl: ''
+  }];
   login = '';
+  count = 9;
+  arrCheck = true;
   constructor(
     private http: HttpClient,
     private router: Router) {}
@@ -21,10 +27,21 @@ export class HomePageComponent implements OnInit {
   goToPage(id) {
     this.router.navigate(['/phone/' + id]);
   }
+  showMorePhones() {
+    return this.http.get('../../assets/phones.json')
+      .subscribe(data => {
+        this.arrPackPhones = Object.values(data).slice(this.count, this.count += 9);
+        this.arrPhones = this.arrPhones.concat(this.arrPackPhones);
+        if (this.arrPackPhones.length < 1) {
+          this.arrCheck = false;
+        }
+      });
+  }
   ngOnInit() {
     return this.http.get('../../assets/phones.json')
       .subscribe(data => {
-        this.arrPhones = Object.values(data);
+        this.arrPhones = Object.values(data).slice(0, this.count);
+        toastr.success('Welcome!');
       });
   }
 }
